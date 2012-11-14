12a60899a368a09cce6d17ef0c8a4a7b0b013902
Billy Brown <druidofluhn@gmail.com>
2012-11-14 09:47:26 +0100

    commenting code

--- Quadratic/simpleController.m
+++ Quadratic/simpleController.m
@@ -21,70 +21,58 @@
 @synthesize radioBtns;
 
 - (IBAction)calculate:(id)sender {
-  
+  // Preferences
   one = false;
-  
   if ([radioBtns selectedColumn] == 1) {
     one = true;
   }
-  
+  // If a is null
   if ([aValue.stringValue isEqualToString:@""] && one == false) {
-    
+    // If text or nothing was entered instead of numbers
     if ((![[NSScanner scannerWithString:bValue.stringValue] scanFloat:NULL] &&
          [cValue.stringValue isEqualToString:@""]) ||
         (![[NSScanner scannerWithString:cValue.stringValue] scanFloat:NULL] &&
          [bValue.stringValue isEqualToString:@""])) {
-      
       error = @"Please enter numbers.";
-      
     } else {
-      
+      // Calculate answer when a = 0
       a = aValue.floatValue;
       b = bValue.floatValue;
       c = cValue.floatValue;
       
       if (b == 0 && c == 0) {
-        
         root1.stringValue = @"";
-        
       } else if (b == 0) {
-        
         root1.stringValue = @"impossible";
-        
       } else if (c == 0) {
-        
         root1.stringValue = @"0";
-        
       } else {
-        
         x1 = -c / b;
         root1.floatValue = x1;
-        
       }
-      
+      // Clearing unneaded fields
       root2.stringValue = @"";
       rhoValue.stringValue = @"";
       sumValue.stringValue = @"";
       productValue.stringValue = @"";
-
     }
-      
+  // If there is no text entered
   } else if (([[NSScanner scannerWithString:aValue.stringValue] scanFloat:NULL] &&
              ([[NSScanner scannerWithString:bValue.stringValue] scanFloat:NULL] ||
               [bValue.stringValue isEqualToString:@""]) && 
              ([[NSScanner scannerWithString:cValue.stringValue] scanFloat:NULL] ||
               [cValue.stringValue isEqualToString:@""])) ||
              ([aValue.stringValue isEqualToString:@""] && one == true)) {
-    
+    // If a field is empty and preference is 1
     if ([aValue.stringValue isEqualToString:@""]) {
       a = 1;
-    } else {
+    } else { // Continue calculation
       a = aValue.floatValue;
     }
     
     b = bValue.floatValue;
     c = cValue.floatValue;
-    
+    // If b is empty
     if ([bValue.stringValue isEqualToString:@""]) {
       if (one == false) {
         b = 0;
@@ -92,16 +80,15 @@
         b = 1;
       }
     }
-    
+    // If c is empty
     if ([cValue.stringValue isEqualToString:@""]) {
       c = 0;
     }
-    
+    // Calculating rho/delta
     rho = (b * b) - (4 * a * c);
     rhoValue.floatValue = rho;
-    
+    // If answers are possible
     if (rho >= 0) {
-      
       x1 = (-b + sqrt(rho)) / (2 * a);
       x2 = (-b - sqrt(rho)) / (2 * a);
       root1.floatValue = x1;
@@ -111,29 +98,22 @@
       sumValue.floatValue = sum;
       productValue.floatValue = product;
       error = @"";
-      
-    } else {
-      
+    } else { // Rho/delta is negative, not possible
       error = @"No roots.";
       root1.stringValue = @"";
       root2.stringValue = @"";
       sumValue.stringValue = @"";
       productValue.stringValue = @"";
-      
     }
     
   } else { // What happens if letters were entered
-      
      error = @"Please enter numbers.";
-      
   }
-  
+  // Print error message
   errorMessage.stringValue = error;
-  
 }
 
 - (IBAction)clear:(id)sender { // Reseting EVERYTHING
-  
   aValue.stringValue = @"";
   bValue.stringValue = @"";
   cValue.stringValue = @"";
@@ -144,7 +124,6 @@
   productValue.stringValue = @"";
   errorMessage.stringValue = @"";
   [aValue selectText:self];
-  
 }
 
 @end
